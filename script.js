3e7679ed7c02b898a162f97b8aa669f57f171dff
Mayara Pinheiro <67695977+maypinheiro@users.noreply.github.com>
2021-05-28 14:00:03 -0300

    .

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const dino = document.querySelector('.dino');
 const background=document.querySelector('.background');
 let isJumping= false;
-
+let position=0;
 function handleKeyUp(event){
      if(event.keyCode === 32 || 38 || 104){
         if(!isJumping){
@@ -15,7 +15,7 @@ function handleKeyUp(event){
 }
 
 function jump(){
-    let position=0;
+    
     isJumping=true;
 
     let upInterval = setInterval(()=>{
@@ -45,18 +45,28 @@ function jump(){
 function createCactus(){
     const cactus =document.createElement('div');
     let cactusPosition=1000;
+    let randonTime=Math.random() * 6000;
+    
+
     cactus.classList.add('cactus');
     cactus.style.left=1000+'px'
     background.appendChild(cactus);
 
     let leftInterval =setInterval(()=> {
-        cactusPosition-=10;
-        cactus.style.left =cactusPosition + 'px';
         if(cactusPosition <-60){
             clearInterval(leftInterval);
             background.removeChild(cactus);
+        } else if(cactusPosition>0 && cactusPosition<60 && position <60) {
+            //Game Over
+           clearInterval(leftInterval);
+           document.body.innerHTML= '<h1 class="game-over">Fim do Jogo</h1>';
+        } else{
+            cactusPosition-=10;
+            cactus.style.left =cactusPosition + 'px';
         }
+        
     },20)
+    setTimeout(createCactus,randonTime);
 }
 createCactus()
 document.addEventListener('keyup',handleKeyUp);
